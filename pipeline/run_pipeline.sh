eadbebfd0c74bb92edc5bc9d0a2a5ccf070b41dc
Marten Hoekstra <marten@dolhuis.nl>
2017-04-13 11:00:47 +0200

    Pipeline files in pdal docker with absolute path

--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -11,7 +11,7 @@ for file in /data/Cloud_Interp_Class/*.laz
 do
   echo 'Next file: '
   echo $file
-  pdal pipeline --debug pdal-connection.json --readers.las.filename=$file --writers.pgpointcloud.table=patches
+  pdal pipeline --debug /pipeline/pdal-connection.json --readers.las.filename=$file --writers.pgpointcloud.table=patches
   let  "COUNT -= 1"
   let "i += 1"
   echo '##### ' $i ' bestanden gedaan, nog' $COUNT 'te gaan.... #####'
