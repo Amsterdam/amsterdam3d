4b8be60e1c81e65832cb572a59f9d171cfda0ca8
Marten Hoekstra <marten@dolhuis.nl>
2017-05-08 16:28:25 +0200

    Correct build order

--- 3dfier/createTileset.js
+++ 3dfier/createTileset.js
@@ -6,19 +6,23 @@ export default function(config) {
 	return new Promise(function(resolve, reject){
 		console.log('Creating tileset.json NEW');
 		fs.readFile('./src/tileset_template.json',function (err,doc){
+			console.log('Reading ./src/tileset_template.json');
 			if (err) return console.error(err);
 			var template = JSON.parse(doc);
 			var tiles = config.tiles;
 
+			console.log('For each tiles');
 			tiles.forEach(function(bbox){
 				//Get lat lon
 				var RD = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs +no_defs";
 				var lowerleft = proj4(RD,'WGS84',[bbox[0], bbox[1]]);
 				var upperright = proj4(RD,'WGS84',[bbox[2], bbox[3]]);
 
+				console.log('Before Cesium');
 				var m = Cesium.Transforms.eastNorthUpToFixedFrame(
 						Cesium.Cartesian3.fromDegrees(lowerleft[0],lowerleft[1], 0.0)
 				);
+				console.log('After Cesium');
 				var boundingVolume = {
 					region: [
 						Cesium.Math.toRadians(lowerleft[0]),Cesium.Math.toRadians(lowerleft[1]),
